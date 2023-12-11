b7d5d2420ab1d775e43988588cd893af93abc731
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2023-12-11 19:13:47 +0530

    Fixed bug
    Unwanted pattern match

--- palmquery.py
+++ palmquery.py
@@ -5,30 +5,34 @@ from dotenv import load_dotenv
 from config import PALM_MODEL, PALM_TEMPERATURE, PALM_CANDIDATE_COUNT, PALM_TOP_K, PALM_TOP_P
 
 load_dotenv()
-
 API_KEY=os.getenv('PALM_API_KEY')
 
+summary_preprompt = """You are a helpful assistant, Summarize the below text content into 10 points. These 10 points should cover most of the content"""
+
+preprompt = """You are a helpful assistant, you will only use the provided CONTEXT to answer user questions.
+Read the given context before answering questions and think step by step. If you can not answer a user question based on the provided context, inform the user. 
+Do not provide false facts. Provide short answers limited to 5 lines for the question."""
+
 def ask_palm(context, query=None, summary=False):
+
+    if summary:
+        prompt = summary_preprompt
+        TEMPERATURE = 0.7
+    else:
+        prompt = preprompt
+        TEMPERATURE = PALM_TEMPERATURE
     
     palm.configure(api_key=API_KEY)
     messages=[]
     defaults = {
         
       'model': PALM_MODEL,
-      'temperature': 0.7,
+      'temperature': TEMPERATURE,
       'candidate_count': PALM_CANDIDATE_COUNT,
       'top_k': PALM_TOP_K,
       'top_p': PALM_TOP_P,
     }
 
-
-    summary_preprompt = """You are a helpful assistant, Summarize the below text content into 10 points. These 10 points should cover most of the content"""
-    
-    preprompt = """You are a helpful assistant, you will only use the provided CONTEXT to answer user questions.
-Read the given context before answering questions and think step by step. If you can not answer a user question based on the provided context, inform the user. 
-Do not provide false facts. Provide short answers limited to 5 lines for the question."""
-    
-    
     p=re.compile("\\n")
     
     if context:
@@ -36,7 +40,6 @@ Do not provide false facts. Provide short answers limited to 5 lines for the que
 
     if summary:
         messages = [ f"""TEXT CONTENT: {cleaned_context} """]
-        preprompt = summary_preprompt
     else:
         messages = [ f"""CONTEXT: {cleaned_context}
                     
@@ -46,12 +49,12 @@ Do not provide false facts. Provide short answers limited to 5 lines for the que
     
     response = palm.chat(
       **defaults,
-      context=preprompt,
+      context=prompt,
       messages=messages
     )
 
     if summary:
-        pattern = re.compile(r'here[\s\S]*', re.IGNORECASE)
+        pattern = re.compile(r'here are 10[\s\S]*', re.IGNORECASE) #Pattern is too rigid
         match = pattern.search(response.messages[len(response.messages)-1]['content'])
         
         if match:    
