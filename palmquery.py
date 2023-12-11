0bf0d737493e106e1f6f2a61beee0341c28d3bb7
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2023-12-11 11:54:07 +0530

    Added Regex

--- palmquery.py
+++ palmquery.py
@@ -15,19 +15,18 @@ def ask_palm(context, query=None, summary=False):
     defaults = {
         
       'model': PALM_MODEL,
-      'temperature': PALM_TEMPERATURE,
+      'temperature': 0.7,
       'candidate_count': PALM_CANDIDATE_COUNT,
       'top_k': PALM_TOP_K,
       'top_p': PALM_TOP_P,
     }
 
 
-    summary_preprompt = """You are a helpful assistant, You will use the provided text content to summarize it as short as possible.
-    Do not make up facts nor do not make any mistakes."""
+    summary_preprompt = """You are a helpful assistant, Summarize the below text content into 10 points. These 10 points should cover most of the content"""
     
     preprompt = """You are a helpful assistant, you will only use the provided CONTEXT to answer user questions.
-Read the given context before answering questions and think step by step. If you can not answer a user question based on 
-the provided context, inform the user. Do not provide false facts. Provide short answers limited to 5 lines for the question."""
+Read the given context before answering questions and think step by step. If you can not answer a user question based on the provided context, inform the user. 
+Do not provide false facts. Provide short answers limited to 5 lines for the question."""
     
     
     p=re.compile("\\n")
@@ -36,8 +35,7 @@ the provided context, inform the user. Do not provide false facts. Provide short
         cleaned_context=p.sub("",context)
 
     if summary:
-        messages = [ f"""TEXT CONTENT: {cleaned_context}
-                    Summarize the above""" ]
+        messages = [ f"""TEXT CONTENT: {cleaned_context} """]
         preprompt = summary_preprompt
     else:
         messages = [ f"""CONTEXT: {cleaned_context}
@@ -51,4 +49,16 @@ the provided context, inform the user. Do not provide false facts. Provide short
       context=preprompt,
       messages=messages
     )
-    return response.last
+
+    if summary:
+        pattern = re.compile(r'here[\s\S]*', re.IGNORECASE)
+        match = pattern.search(response.messages[len(response.messages)-1]['content'])
+        
+        if match:    
+            extract = match.group(0)
+            return extract
+        else:
+            return "Please Retry"
+    else:
+        return response.last
+    
