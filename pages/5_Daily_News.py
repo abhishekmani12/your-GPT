6388839aec32567e26081fcd975e4d3578b67495
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2024-01-16 11:03:06 +0530

    Added form

--- pages/5_Daily_News.py
+++ pages/5_Daily_News.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.markdown("### Enter your keyword")
 
 summary_flag=False
-@st.cache_resource
+@st.cache_resource(show_spinner="Fetching News Articles")
 def get_cache_news(term, count):
     df=get_news(term, count)
     return df
@@ -14,21 +14,23 @@ def get_cache_news(term, count):
 count = st.sidebar.slider(label="Count", min_value = 1, max_value=100, value=10)
 st.sidebar.header("Get your daily news and summarize them")
 
-term = st.text_input(" ")
-summary_flag = st.checkbox("Summarize (5)")
+with st.form("getn_news"):
 
-if st.button("Get News") and term:
-    df = get_cache_news(term, count)
-    st.dataframe(df['Text'], hide_index=True, use_container_width=True)
+    term = st.text_input(" ")
+    summary_flag = st.checkbox("Summarize (5)")
 
-    st.divider()
-    if summary_flag:
+    if st.form_submit_button("Get News") and term:
+        df = get_cache_news(term, count)
+        st.dataframe(df['Text'], hide_index=True, use_container_width=True)
 
-        with st.spinner("Creating Summaries ..."):
-            tabbers = st.tabs([str(i) for i in range(1,6)])
-            for idx in range(5):
-                tabbers[idx].header(f"{term} - Summary ({idx+1})")
-                tabbers[idx].write(ask_palm(df.iloc[idx,1], summary=True))
+        st.divider()
+        if summary_flag:
+
+            with st.spinner("Creating Summaries ..."):
+                tabbers = st.tabs([str(i) for i in range(1,6)])
+                for idx in range(5):
+                    tabbers[idx].header(f"{term} - Summary ({idx+1})")
+                    tabbers[idx].write(ask_palm(df.iloc[idx,1], summary=True))
 
             
 
