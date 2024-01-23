7c885b4fbc109b7d25276829a945b73057c82b71
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2024-01-23 13:45:05 +0530

    qdrant loader

---
+++ qdb.py
@@ -0,0 +1,81 @@
+from qdrant_client import QdrantClient
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import CSVLoader, TextLoader, PyMuPDFLoader, Docx2txtLoader
+
+CHUNK_SIZE=256
+CHUNK_OVERLAP=32
+
+ext2loader = {
+    ".csv": (CSVLoader, {}),
+    ".docx": (Docx2txtLoader, {}),
+    ".pdf": (PyMuPDFLoader, {}),
+    ".txt": (TextLoader, {"encoding": "utf8"}),
+}
+
+def load_document(file_path, existing_files):
+
+    split_ls = file_path.rsplit(".")
+    extension = "." + split_ls[1]
+    fname = split_ls[0].split("/")[-1]
+
+    if extension in ext2loader:
+        if fname not in existing_files:
+
+            loader_type, loader_args = ext2loader[extension]
+            loader = loader_type(file_path, **loader_args)
+            load=loader.load()
+            return load, fname
+        else:
+            return None, fname
+
+    raise ValueError(f" '{extension}' file type not supported")
+
+def split_document(file_path, existing_files=[]):
+
+    document, fname = load_document(file_path, existing_files)
+    if not document:
+        print(f"Vector Embeddings for FILE:{fname} already exists")
+        return None, None
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+    split_text = text_splitter.split_documents(document)
+
+    text_string=""""""
+    for doc in split_text:
+      text_string += doc.page_content
+
+    print("Document Split")
+    return text_string, fname
+
+
+def qvdb_embed(db_path, collection, fpath):
+
+  client = QdrantClient(path=db_path)
+
+  exist=True
+  existing_docs=[]
+
+  try:
+    list_docs = client.query(collection_name=collection, query_text="")
+  except ValueError:
+    print("Collection does not exist, creating new collection")
+    exist=False
+
+  if exist:
+    existing_docs=[]
+    for doc in list_docs:
+      existing_docs.append(doc.metadata.get('source'))
+
+  text, fname = split_document(fpath, existing_docs)
+  if text:
+    metadata = [{"source": fname}]
+
+    client.add(
+        collection_name=collection,
+        documents=[text],
+        metadata=metadata
+    )
+
+    print("Document Embedded and Stored in Vector DB")
+
+  client.close()
